cb66f76371adf5b96963d179c7320eaae3aa56f8
aweujasN <koumae893@gmail.com>
2025-09-12 08:06:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -207,7 +207,7 @@ function displayWords(words) {
 function setupEventListeners() {
     const container = document.getElementById('word-container');
     const searchBox = document.getElementById('search-box');
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyOdQDL7o3ZzZ5vlop8pgOqi53I4ieDa6g-5xaorNqbbbKGwPno8zG221RWMsuPT6gW/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyXYQ3iSMqCZigJdQYe5CIzL853edORW-o7hTElKSC-ODZAeHSDnP9ND5KcFLM217Ni/exec';
 
     searchBox.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
@@ -310,4 +310,5 @@ function createTagButton(text) {
 function updateActiveButton(activeBtn) {
     document.querySelectorAll('.tag-btn').forEach(btn => btn.classList.remove('active'));
     activeBtn.classList.add('active');
+
 }
